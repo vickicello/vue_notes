6910e73597482fd59141db3b0f5d1525991c7996
Vicki Hamm <vhamm@covermymeds.com>
2020-05-29 16:42:41 -0400

    Add QuestionBox layout and styling

--- src/components/QuestionBox.vue
+++ src/components/QuestionBox.vue
@@ -1,3 +1,18 @@
 <template>
-  <h1>Question Box</h1>
+  <div class="question-box-container">
+    <b-jumbotron>
+      <template v-slot:lead>
+        Our question will go here
+      </template>
+
+      <hr class="my-4">
+
+      <p>
+        List of possible answers will go here
+      </p>
+
+    <b-button variant="primary" href="#">Submit</b-button>
+    <b-button variant="success" href="#">Next Question</b-button>
+    </b-jumbotron>
+  </div>
 </template>
