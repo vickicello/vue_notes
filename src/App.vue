03e02a6fe387b9fbd9dec4079c3e08ecd8180eb2
Vicki Hamm <vhamm@covermymeds.com>
2020-05-29 16:21:15 -0400

    Add bootstrap and header

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <Header />
+    <QuestionBox />
   </div>
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue'
+import QuestionBox from './components/QuestionBox.vue'
 
 export default {
   name: 'App',
   components: {
-    // HelloWorld
+    Header,
+    QuestionBox
   }
+  // we need to add components here to be able to access them in the template section above
 }
 </script>
 
